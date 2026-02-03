c1da02ee127450d38f39135ec99a5625433005b3
Kavindu Mihiranga <133510084+WWKMihiranga@users.noreply.github.com>
2026-02-03 07:41:01 +0530

    Added main functional file
    This notebook has the main functionalities of the stutter detection project

---
+++ notebooks/src/utils/json_utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+import torch
+
+def make_json_serializable(obj):
+    """Recursively convert numpy / torch types to native Python types"""
+    if isinstance(obj, dict):
+        return {k: make_json_serializable(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [make_json_serializable(v) for v in obj]
+    elif isinstance(obj, tuple):
+        return [make_json_serializable(v) for v in obj]
+    elif isinstance(obj, np.integer):
+        return int(obj)
+    elif isinstance(obj, np.floating):
+        return float(obj)
+    elif isinstance(obj, torch.Tensor):
+        return obj.item() if obj.numel() == 1 else obj.cpu().tolist()
+    else:
+        return obj
